508972b4127760295e3d07a95cc40359bb58fd6a
Shazzad501 <assazzad501@gmail.com>
2025-01-31 12:06:16 +0600

    fetch data into the meal db

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,9 +1,44 @@
+"use client"
+import { useEffect, useState } from "react";
 
 
 const MealsPage = () => {
+  const [meal, setMeal] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const getMeals = async()=>{
+    try{
+      const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+      const data = await res.json();
+      return setMeal(data?.meals || [])
+    }
+    catch(error){
+      console.log(error);
+      return [];
+    }
+  }
+
+  useEffect(()=>{
+    getMeals();
+  }, [search])
   return (
     <div>
-      <h2>Meals page</h2>
+      <h2 className="font-bold text-2xl text-center mb-5">Search Meal by name!</h2>
+      <div className="flex flex-col items-center justify-center mb-6">
+        <input type="text" value={search} onChange={(e)=> setSearch(e.target.value)} placeholder="Search meal" className="p-2 rounded-md text-black"/>
+      </div>
+      <div className="grid grid-cols-4 gap-5">
+        {
+          meal?.map(singleMeal=>{
+            return(
+              <div className="border border-white p-5 rounded-lg">
+                <p className="text-lg font-bold">{singleMeal?.strMeal}</p>
+                <p className="text-base font-bold">{singleMeal?.strInstructions}</p>
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
   );
 };
